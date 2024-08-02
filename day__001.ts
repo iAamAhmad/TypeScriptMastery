72dc93f93e1f96120e303c0125387d2206d69007
iAamAhmad <ahmad441026@gmail.com>
2024-08-02 06:22:03 -0700

    first commit

---
+++ day__001.ts
@@ -0,0 +1,8 @@
+let count = 1;
+let total = 0;
+while(count <= 10){
+  total += count
+  count += 1
+}
+
+console.log("Total Will Be Printed here : ", total)
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let count1 = 1;
+let total1 = 0;
+while(count1 <= 10){
+  total1 += count1
+  count1 += 1
+}
+
+console.log("Total Will Be Printed here : ", total1);
+console.log(sum(range(1,10)));
