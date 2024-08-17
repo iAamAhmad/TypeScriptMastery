5d25afc86d742ca08d2e9dea41b1c80cfbbcaea0
iAamAhmad <ahmad441026@gmail.com>
2024-08-17 05:17:05 -0700

    functions for practice

--- functions.js
+++ functions.js
@@ -10,7 +10,7 @@ addMe(2, 111)
 
 // function expression / anonymous
 
-const addMe2 = function(a, b) {
+const addMe2 = function (a, b) {
   console.log("Addition : ", a + b)
 }
 
@@ -20,13 +20,26 @@ addMe2(12, 13)
 
 // arrow functions
 
-const addMe3 = (a , b = 2) => {
+const addMe3 = (a, b = 2) => {
   console.log("Addition :", a + b)
 }
 addMe3(5)
 
 
 function myFunction(a, b) {
-    return a * b;
+  return a * b;
 }
-console.log(myFunction(1,2))
+console.log(myFunction(1, 2))
+
+
+
+function callBackAddition(d, a, callBack) {
+  const result = a + b
+  callBack(result)
+}
+
+// function addedCallBack(val) {
+//   console.log("🚀 ~ callBackAddition ~ val:", val)
+// }
+// callBackAddition(2, 5, addedCallBack)
+callBackAddition(2, 5, (val) => console.log(val))
